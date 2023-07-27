71f16e2b016f82cc28915534d3bfef6baf2689a9
marcelomrad <marcelomrad@gmail.com>
2023-07-27 15:29:05 -0300

    feat: config inicial

---
+++ practicall_challange/src/main/java/com/example/practicall_challange/PracticallChallangeApplication.java
@@ -0,0 +1,13 @@
+package com.example.practicall_challange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticallChallangeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticallChallangeApplication.class, args);
+    }
+
+}
---
+++ practicall_challange/src/test/java/com/example/practicall_challange/PracticallChallangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practicall_challange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticallChallangeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
